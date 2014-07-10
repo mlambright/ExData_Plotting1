8bb916746d0de6300f1279c54ec98d123511282c
mlambright <michaelglambright@gmail.com>
2014-07-10 01:15:36 -0400

    Add plot 3

---
+++ plot3.R
@@ -0,0 +1,14 @@
+run <- function() {
+  housePower <- read.table('household_power_consumption.txt', header=TRUE, sep=';', stringsAsFactors=FALSE)
+  allowedDates <- c('1/2/2007', '2/2/2007')
+  housePower <- housePower[housePower$Date %in% allowedDates,]
+  housePower$Date <- strptime(paste(housePower$Date, housePower$Time), '%d/%m/%Y %H:%M:%S')
+  
+  png('plot3.png')
+  plot(housePower$Date, y=as.numeric(housePower$Sub_metering_1), xlab='', ylab='Energy sub metering', type='n')
+  lines(housePower$Date, as.numeric(housePower$Sub_metering_1))
+  lines(housePower$Date, as.numeric(housePower$Sub_metering_2), col='red')
+  lines(housePower$Date, as.numeric(housePower$Sub_metering_3), col='blue')
+  legend('topright', paste('Sub_metering',1:3,sep='_'), lwd=rep(1,3), col=c('black','red','blue'))
+  dev.off()
+}
