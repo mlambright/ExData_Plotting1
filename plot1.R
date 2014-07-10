1890f4cdbdaee062d92556523c6dab314ed01456
mlambright <michaelglambright@gmail.com>
2014-07-10 00:39:22 -0400

    Add plot 1

---
+++ plot1.R
@@ -0,0 +1,18 @@
+run <- function() {
+  housePower <- read.table('household_power_consumption.txt', header=TRUE, sep=';', stringsAsFactors=FALSE)
+  housePower$Date <- strptime(housePower$Date, '%d/%m/%Y')
+  allowedDates <- strptime(c('1/2/2007', '2/2/2007'),'%d/%m/%Y')
+  housePower <- housePower[housePower$Date %in% allowedDates,]
+  
+  png('plot1.png')
+  hist(as.numeric(housePower$Global_active_power), col='red', 
+       xlab='Global Active Power (kilowatts)', ylab='Frequency', 
+       main='Global Active Power', breaks=seq(0,8,0.5), 
+       axes=F)
+  axis(1, at=seq(0,6,2))
+  axis(2, at=seq(0,1200,200))
+  
+  dev.off()
+}
+
+
